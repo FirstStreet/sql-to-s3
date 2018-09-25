0e33055034fa8250de34d7eedceef9b22c85ca5a
Daniel Seripap <daniel@seripap.com>
2018-09-25 15:57:34 -0400

    Fix import script

--- import.sh
+++ import.sh
@@ -6,9 +6,6 @@ PASS=${PASS:-${PASS}}
 BUCKET=${BUCKET:-${BUCKET}}
 OBJECT_NAME=${OBJECT_NAME:-${OBJECT_NAME}}
 
-echo "Pulling Database from $HOST"
-mysqldump --add-drop-table --host=$HOST --user=$USER --password=$PASS $DB_NAME --result-file=$OBJECT_NAME --verbose
-
 echo "Importing $OBJECT_NAME from $BUCKET to $HOST"
 aws s3 cp $BUCKET/$OBJECT_NAME $OBJECT_NAME
-mysql --host=$HOST --user=$USER --password=$PASS $OBJECT_NAME < $OBJECT_NAME
+mysql --host=$HOST --user=$USER --password=$PASS $DB_NAME < $OBJECT_NAME
